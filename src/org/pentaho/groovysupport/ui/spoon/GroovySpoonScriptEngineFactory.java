c7a2550a8b0e5a373d0ece8d33da55615f1801b5
Matt Burgess <mburgess@pentaho.com>
2012-12-05 08:47:20 -0500

    Filled in ScriptEngineFactory methods

--- src/org/pentaho/groovysupport/ui/spoon/GroovySpoonScriptEngineFactory.java
+++ src/org/pentaho/groovysupport/ui/spoon/GroovySpoonScriptEngineFactory.java
@@ -7,7 +7,13 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineFactory;
 
 public class GroovySpoonScriptEngineFactory implements ScriptEngineFactory {
-
+	
+	private static final String languageName = "Groovy";
+	private static final String languageVersion = "1.8.x";
+	private static final List<String> extensions = Arrays.asList("groovy");
+	private static final List<String> mimeTypes = Arrays.asList("application/x-groovy");
+	private static final List<String> names = Arrays.asList("groovyspoon","GroovySpoon");
+	
 	@Override
 	public String getEngineName() {
 		return "groovyspoon";
@@ -20,58 +26,84 @@ public class GroovySpoonScriptEngineFactory implements ScriptEngineFactory {
 
 	@Override
 	public List<String> getExtensions() {
-		return Arrays.asList("groovy");
+		return extensions;
 	}
 
 	@Override
 	public List<String> getMimeTypes() {
-		// TODO Auto-generated method stub
-		return null;
+		return mimeTypes;
 	}
 
 	@Override
 	public List<String> getNames() {
-		
-		return Arrays.asList("groovyspoon","GroovySpoon");
+		return names;
 	}
 
 	@Override
 	public String getLanguageName() {
-		return "Groovy";
+		return languageName;
 	}
 
 	@Override
 	public String getLanguageVersion() {
-		return "1.8.x";
+		return languageVersion;
 	}
 
 	@Override
 	public Object getParameter(String key) {
-		// TODO Auto-generated method stub
+		if(key.equals(ScriptEngine.ENGINE)) {
+			return getEngineName();
+		}
+		else if(key.equals(ScriptEngine.ENGINE_VERSION)) {
+			return getEngineVersion();
+		}
+		else if(key.equals(ScriptEngine.NAME)) {
+			return getNames();
+		}
+		else if(key.equals(ScriptEngine.LANGUAGE)) {
+			return getLanguageName();
+		}
+		else if(key.equals(ScriptEngine.LANGUAGE_VERSION)) {
+			return getLanguageVersion();
+		}
+		// Add any implementation-specific params here
+		
 		return null;
 	}
 
 	@Override
 	public String getMethodCallSyntax(String obj, String m, String... args) {
-		// TODO Auto-generated method stub
-		return null;
+		String ret = obj;
+	      ret += "." + m + "(";
+	      for (int i = 0; i < args.length; i++) {
+	          ret += args[i];
+	          if (i == args.length - 1) {
+	              ret += ")";
+	          } else {
+	              ret += ",";
+	          }
+	      }
+	      return ret;
 	}
 
 	@Override
 	public String getOutputStatement(String toDisplay) {
-		// TODO Auto-generated method stub
-		return null;
+		return "println '" + toDisplay + "'";
 	}
 
 	@Override
 	public String getProgram(String... statements) {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder();
+		for(String statement : statements) {
+			sb.append(statement);
+			sb.append(";"); // Not required in Groovy but helpful to the parser
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 	@Override
 	public ScriptEngine getScriptEngine() {
 		return new GroovySpoonScriptEngine();
 	}
-
 }
